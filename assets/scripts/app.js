a35763ab4b7b4b6404b901d5c55e58e1e1cdc47e
Shlok Kaushik <54725967+ShlokKaushik23@users.noreply.github.com>
2020-09-03 11:54:52 +0600

    Update

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,3 +1,26 @@
+// Devfolio Button
+document.addEventListener('DOMContentLoaded', function () {
+    let devfolioOptions = {
+        buttonSelector: '#devfolio-apply-now',
+        key: 'myhackathonkey',
+    }
+
+    let script = document.createElement('script');
+    script.src = "https://apply.devfolio.co";
+    document.head.append(script);
+
+    script.onload = function () {
+        new Devfolio(devfolioOptions);
+    }
+
+    script.onerror = function () {
+        document.querySelector(devfolioOptions.buttonSelector).addEventListener('click', function () {
+            window.location.href = 'https://devfolio.co/external-apply/' + devfolioOptions.key;
+        });
+    }
+});
+
+
 
 $(document).ready(function(){
 
