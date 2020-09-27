4e4898f0055c7128ae0743ba2f3a6164b7325b50
ShlokKaushik23 <54725967+ShlokKaushik23@users.noreply.github.com>
2020-09-28 02:48:44 +0600

    Updated

--- assets/scripts/countdowntimer.js
+++ assets/scripts/countdowntimer.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("Sept 29, 2020 00:00:00").getTime();
+var countDownDate = new Date("Sept 29, 2020 10:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
